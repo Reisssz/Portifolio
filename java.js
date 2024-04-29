95e1b79970722b94fecf06c70080fde0507758bf
Pedro Reis <pedrinzx7896@gmail.com>
2024-04-29 14:53:12 -0300

    version3

--- java.js
+++ java.js
@@ -1,12 +1,1 @@
-
-$(document).ready(function () {
-  $('a[href^="#"]').on('click', function (event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-      event.preventDefault();
-      $('html, body, footer').stop().animate({
-        scrollTop: target.offset().top
-      }, 999); // Tempo de animação em milissegundos (1000 = 1 segundo)
-    }
-  })
-})
+/*window.prompt('Esta Pronto ?')*/
